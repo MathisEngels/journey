7c29cbdda8fd2722d8c515031e52f9b551b8ed53
Mathis Engels <mathisengels@yahoo.fr>
2024-02-06 23:44:37 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
+
+import mdx from '@astrojs/mdx'
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        react(),
+        tailwind({
+            applyBaseStyles: false,
+        }),
+        mdx(),
+    ],
+})
