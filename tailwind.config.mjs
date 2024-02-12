32ab4ef98592749e91373c78de42000cd73d1863
Mathis Engels <mathisengels@yahoo.fr>
2024-02-12 10:51:39 +0100

    Add GSAP, some shadcn-UI's component, lucide icons and r3f-perf

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -61,12 +61,21 @@ module.exports = {
                     from: { height: 'var(--radix-accordion-content-height)' },
                     to: { height: '0' },
                 },
+                'bounce-rl': {
+                    '0%, 100%': {
+                        transform: 'translateX(12%)',
+                    },
+                    '50%': {
+                        transform: 'translateX(0%)',
+                    },
+                },
             },
             animation: {
                 'accordion-down': 'accordion-down 0.2s ease-out',
                 'accordion-up': 'accordion-up 0.2s ease-out',
+                'bounce-rl': 'bounce-rl 1s cubic-bezier(0.37, 0, 0.63, 1) infinite',
             },
         },
     },
     plugins: [require('tailwindcss-animate')],
-}
+};
