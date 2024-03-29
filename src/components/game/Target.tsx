229b3391079e83281cfcc34340aaa4673d802d75
Mathis Engels <mathisengels@yahoo.fr>
2024-03-29 18:22:22 +0100

    feat: rename target object

--- src/components/game/Target.tsx
+++ src/components/game/Target.tsx
@@ -5,17 +5,19 @@ import { useEffect } from 'react';
 import type { Mesh } from 'three';
 
 function Target() {
-    const { nodes, materials } = useGLTF('/target-c.glb');
+    const { nodes, materials } = useGLTF('/target.glb');
 
     const $targetPosition = useStore(targetPosition);
 
     useEffect(() => {
         return () => {
-            useGLTF.clear('/target-c.glb');
+            useGLTF.clear('/target.glb');
         };
     }, []);
 
     return <mesh geometry={(nodes.Target as Mesh).geometry} material={materials.nodes} position={$targetPosition} />;
 }
+
+useGLTF.preload('/target.glb');
 
 export default Target;
