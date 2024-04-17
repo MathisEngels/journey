58d632412fb18aa14a6203925ee28b3a0024127f
Mathis Engels <mathisengels@yahoo.fr>
2024-04-17 10:52:03 +0200

    feat: add robot integration for robots.txt file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import robots from 'astro-robots';
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,5 +19,6 @@ export default defineConfig({
         }),
         mdx(),
         sitemap(),
+        robots(),
     ],
 });
